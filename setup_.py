c6acd42e73832089baa22f439dd446d0270b7147
Semyon Yakimov <sdyakimov@gmail.com>
2022-05-13 19:50:15 +0300

    Fix building

--- setup_.py
+++ setup_.py
@@ -7,7 +7,7 @@ if sys.platform == 'win32':
     base = 'Win32GUI'
 
 include_files = [config.LOCALE_DIR, config.TEMPLATE_DIR, config.IMG_DIR, config.SOUND_DIR, config.base_dir('LICENSE'),
-                 config.base_dir('changelog.md'), config.base_dir('changelog_ru.md'),
+                 config.base_dir('changelog.en.md'), config.base_dir('changelog.ru.md'),
                  config.base_dir('configs'), config.SCRIPT_DIR, config.STYLE_DIR]
 includes = ['atexit', 'codecs']
 excludes = ['Tkinter']
