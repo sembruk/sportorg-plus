923f860d6cf0303cff7e14cc088c81a86f37f1e6
Semyon Yakimov <sdyakimov@gmail.com>
2024-11-02 21:57:25 +0300

    Get person from SQLite database: debug

--- sportorg/modules/sportident/result_generation.py
+++ sportorg/modules/sportident/result_generation.py
@@ -108,30 +108,34 @@ class ResultSportidentGeneration:
     def _find_person_by_result(self):
         if self._person:
             return True
+
+        name,surname,year_of_birth,team_name,group_name = get_person_info_by_card_number_from_db(self._result.card_number)
+        if name and surname and year_of_birth and team_name and group_name:
+            person = find(race().persons, name=name, surname=surname, year_of_birth=year_of_birth)
+            if person:
+                person.card_number = self._result.card_number
+                person.bib = self._result.card_number
+                self._person = person
+                return True
+
+            self._person = Person()
+            self._person.bib = self._result.card_number
+            self._person.name = name
+            self._person.surname = surname
+            self._person.set_year(year_of_birth)
+            team = race().add_new_team(team_name)
+            team.name = team_name
+            self._person.team = team
+            self._person.group = find(race().groups, name=group_name)
+            logging.info('New person: {}'.format(self._person))
+
+            #self._result.person = self._person
+            race().persons.append(self._person)
+            return True
+
         for person in race().persons:
             if person.card_number and person.card_number == self._result.card_number:
                 self._person = person
-                return True
-            name,surname,year_of_birth,team_name,group_name = get_person_info_by_card_number_from_db(self._result.card_number)
-            if name and surname and year_of_birth and team_name and group_name:
-                person = find(race().persons, name=name, surname=surname, year_of_birth=year_of_birth)
-                if person:
-                    self._person = person
-                    return True
-
-                self._person = Person()
-                self._person.bib = self._result.card_number
-                self._person.name = name
-                self._person.surname = surname
-                self._person.set_year(year_of_birth)
-                team = race().add_new_team(team_name)
-                team.name = team_name
-                self._person.team = team
-                self._person.group = find(race().groups, name=group_name)
-                logging.info('New person: {}'.format(self._person))
-
-                #self._result.person = self._person
-                race().persons.append(self._person)
                 return True
 
         return False
