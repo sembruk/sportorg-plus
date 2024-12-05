ff1cd6651b6297d308c3ebe1fb6880c3fb68525c
Semyon Yakimov <sdyakimov@gmail.com>
2024-12-05 18:27:48 +0300

    test: IOF XML parser

---
+++ tests/test_iof_parse_files.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+import pytest
+
+from sportorg.libs.iof.parser import parse
+
+
+@pytest.mark.parametrize(
+    'file_name',
+    [
+        'competitorList.xml',
+        'entryList.xml',
+        'entryList2.xml',
+        'resultList.xml',
+        'resultList_ok.xml',
+        'startList.xml',
+    ],
+)
+def test_parse(file_name):
+    assert parse(Path('tests/data/iof') / file_name)
