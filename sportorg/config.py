63d02b881df5d6437f6d49e99fa5781aa9cdbd5a
Semyon Yakimov <sdyakimov@gmail.com>
2025-01-20 20:47:54 +0300

    Release v1.5

--- sportorg/config.py
+++ sportorg/config.py
@@ -14,7 +14,7 @@ class Env(BaseSettings):
         env_file = '.env'
 
 NAME = 'SportOrgPlus'
-VERSION = Version('v1.5.0-beta')
+VERSION = Version('v1.5.0')
 REPO_BASE = 'sembruk/sportorg-plus'
 REPO_URL = 'https://github.com/' + REPO_BASE
 
