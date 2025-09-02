230d6492387832aca6ea887e82589d14f044da32
Semyon Yakimov <sdyakimov@gmail.com>
2025-09-02 22:48:20 +0300

    Change version

--- sportorg/config.py
+++ sportorg/config.py
@@ -14,7 +14,7 @@ class Env(BaseSettings):
         env_file = '.env'
 
 NAME = 'SportOrgPlus'
-VERSION = Version('v1.5.1')
+VERSION = Version('v1.5.2')
 REPO_BASE = 'sembruk/sportorg-plus'
 REPO_URL = 'https://github.com/' + REPO_BASE
 
